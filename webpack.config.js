7a2c5d7613f622bc1ff5a01484df05b551032afe
ssoares <savio_soares@gap.com>
2020-06-08 09:49:28 -0300

    fixing webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.css$/i, use: ['style-loader', 'css-loader']}
-            ]
-    },
-    mode: 'development',
-    devtool: "#source-map"
+module.exports = () => {
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.join(__dirname, 'public'),
+            publicPath: '/',
+        },
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif|ico|json)$/,
+                    exclude: /node_modules/,
+                    use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+                }
+                ]
+        },
+        devtool: "#source-map",
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            compress: true,
+            port: 9000,
+            historyApiFallback: true
+        }
+    };
 };
