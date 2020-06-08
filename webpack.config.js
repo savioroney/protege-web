fb146d8d40261be38e390e7c92089aac9401039a
ssoares <savio_soares@gap.com>
2020-06-08 10:09:22 -0300

    foo

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = () => {
                 {
                     test: /\.(png|svg|jpg|jpeg|gif|ico|json)$/,
                     exclude: /node_modules/,
-                    use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
+                    use: ['file-loader?name=[name].[ext]']
                 }
                 ]
         },
